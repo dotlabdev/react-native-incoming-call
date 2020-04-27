2e39a466e99ddce557557ef845b0a0eab5926126
Bojke <danijelbojcic@gmail.com>
2020-04-27 11:21:42 +0200

    added unlockDevice method

--- android/src/main/java/com/incomingcall/IncomingCallModule.java
+++ android/src/main/java/com/incomingcall/IncomingCallModule.java
@@ -1,12 +1,19 @@
 package com.incomingcall;
 
+import android.os.PowerManager;
+import android.app.KeyguardManager;
+import android.app.KeyguardManager.KeyguardLock;
 import android.content.Intent;
 import android.app.Activity;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.UiThreadUtil;
+
 
 public class IncomingCallModule extends ReactContextBaseJavaModule {
 
@@ -54,5 +61,36 @@ public class IncomingCallModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void exitApp() {
         android.os.Process.killProcess(android.os.Process.myPid());
+    }
+
+    @ReactMethod
+    public void unlockDevice() {
+        UiThreadUtil.runOnUiThread(new Runnable() {
+            public void run() {
+                Activity mCurrentActivity = getCurrentActivity();
+                if (mCurrentActivity == null) {
+                    return;
+                }
+                KeyguardManager keyguardManager = (KeyguardManager) reactContext.getSystemService(reactContext.KEYGUARD_SERVICE);
+
+                KeyguardLock keyguardLock = keyguardManager.newKeyguardLock(reactContext.KEYGUARD_SERVICE);
+                keyguardLock.disableKeyguard();
+
+                PowerManager powerManager = (PowerManager) reactContext.getSystemService(reactContext.POWER_SERVICE);
+                PowerManager.WakeLock wakeLock = powerManager.newWakeLock(
+                        PowerManager.FULL_WAKE_LOCK
+                                | PowerManager.ACQUIRE_CAUSES_WAKEUP
+                                | PowerManager.SCREEN_BRIGHT_WAKE_LOCK
+                                | PowerManager.ON_AFTER_RELEASE, "IncomingCallModule:mywake");
+
+                wakeLock.acquire();
+
+                Window window = mCurrentActivity.getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+            }
+        });
     }
 }
